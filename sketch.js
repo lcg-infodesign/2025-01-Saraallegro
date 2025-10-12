4c722d3f2cdfab6e8eab9d634696f9da0ce7c7d9
Saraallegro <sara.allegro@mail.polimi.it>
2025-10-12 15:01:08 +0200

    sistemo regole

--- sketch.js
+++ sketch.js
@@ -22,35 +22,35 @@ function preload() {
 
 // funzione media
 function calcAvg(arr) {
-  if (arr.lenght === 0) return 0; // evita la divisione per lo 0
+  if (arr.length === 0) return 0; // evita la divisione per lo 0
   let sum = 0;
-  for (let i = 0; i < arr.lenght; i++) {
+  for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
-  return sum / arr.lenght;
+  return sum / arr.length;
 }
 
 // funzione per calcolare la deviazione di un array
 function calcStD(arr) {
-  if (arr.lenght === 0) return 0;
-  let avarage= calcAvg(arr); 
+  if (arr.length === 0) return 0;
+  let average= calcAvg(arr); 
   let sommaQuad = 0;
-  for (let i = 0; i < arr.lenght; i++) {
+  for (let i = 0; i < arr.length; i++) {
     sommaQuad += (arr[i] - average) ** 2;
   }
-  let variation = sommaQuad / arr.lenght;
+  let variation = sommaQuad / arr.length;
   return Math.sqrt(variation);
 }
 
 // funzione per calcolare la moda
 function calcMode(arr) {
-  if (arr.lenght === 0) return null;
+  if (arr.length === 0) return null;
   let counts = {}; // conta le occorrenze
   let maxCount = 0;
   let mode = []
 
 // conta le occorrenze di ogni valore
-for (let i = 0; i < arr.lenght; i++) {
+for (let i = 0; i < arr.length; i++) {
   let val = arr[i];
   counts[val] = (counts[val] || 0) + 1;
   if (counts[val] > maxCount) {
@@ -66,17 +66,17 @@ for (let key in counts) {
 }
 
 // se c'è una sola moda restituisce il valore singolo
-if (mode.lenght === 1) return mode[0];
+if (mode.length === 1) return mode[0];
 return mode; // sennò ritorna un array di valori moda
 }
 
 // funzione per calcolare la mediana
 function calcMedian(arr) {
-  if (arr.lenght === 0) return 0; // se l'array è vuoto ridà zero
+  if (arr.length === 0) return 0; // se l'array è vuoto ridà zero
   let sorted = arr.slice().sort((a, b) => a - b); // ordina senza modificare l'array originale
-  let middle = Math.floor(sorted.lenght / 2);
+  let middle = Math.floor(sorted.length / 2);
 
-  if(sorted.lenght % 2 === 0) {
+  if(sorted.length % 2 === 0) {
   // se c'è un numero pari di elementi pari di elementi calcola la media dei due valori centrali
   return (sorted[middle - 1] + sorted [middle]) / 2;
   } else {
@@ -89,12 +89,13 @@ function setup() {
   createCanvas(400, 400);
   // ciclo che si ripete su tutte le righe
   for (let r = 0; r < table.getRowCount(); r++) {
+  const col3 = table.getNum(r, 3); // perchè lavoro sulla colonna 3
   const col2 = table.getNum(r, 2); // perchè lavoro sulla colonna 2
 
   // applico le regole
-  if (col2 > 60 && col2 < 0) { // per integrare enrambe le condizioni delle regole
+  if (col3 > 60 && col2 < 0) { // per integrare enrambe le condizioni delle regole
   let rowObj = {}; // crea un oggetto per ogni riga valida 
-  for (let c = 0; c < table.getColumncount(); c++) {
+  for (let c = 0; c < table.getColumnCount(); c++) {
     let colName = table.columns[c]; // nome colonna
     rowObj[colName] = table.get(r, c); // per salvare riga e colonna insieme (riga-colonna)
   }
@@ -103,20 +104,15 @@ function setup() {
 }
 
 // estrarre i valori numerici dalle colonne
-col0Values = filteredRows.map(row=> Number(row.column0));
-col1Values = filteredRows.map(row=> Number(row.column1));
-col2Values = filteredRows.map(row=> Number(row.column2));
-col3Values = filteredRows.map(row=> Number(row.column3));
-col4Values = filteredRows.map(row=> Number(row.column4));
+col0Values = filteredRows.map(row => Number(row.column0));
+col1Values = filteredRows.map(row => Number(row.column1));
+col2Values = filteredRows.map(row => Number(row.column2));
+col3Values = filteredRows.map(row => Number(row.column3));
+col4Values = filteredRows.map(row => Number(row.column4));
 
 // f01. calcolo media colonna 0
 average0 = calcAvg(col0Values);
-print("average col0:", average0);
-
-// disegnare il grafico di avg colonna 0
-drawAvarageCol0();
-
-// f02.
+console.log("average col0:", average0);
 }
 
 function draw() {
